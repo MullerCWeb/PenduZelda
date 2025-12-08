e155125ba4f31f042ea74b27f229a2cef4696721
MullerCWeb <112878233+MullerCWeb@users.noreply.github.com>
2025-12-08 17:52:19 +0100

    Fin

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -39,7 +39,106 @@ public class GameControllerScript : MonoBehaviour
         _attemptsLeftCount = MaxErrors;
         attemptsLeft.text = attemptsLeftLabel + _attemptsLeftCount.ToString();
         
-        //TODO Handle input field and button reactivation
+        EmptyLetterInput();
+        submitButton.interactable = true;
+
+    }
+
+    void EmptyLetterInput()
+    {
+        letterInput.text = "";
+        letterInput.interactable = true;
+        letterInput.ActivateInputField();
+    }
+
+    public void CheckLetterFromInput()
+    {
+        string input = letterInput.text.Trim().ToUpper();
+
+        EmptyLetterInput();
+
+        bool inputIsValid = ValidateInput(input);
+        if (!inputIsValid)
+        {
+            // On stoppe la vérification parce que la lettre est invalide
+            return;
+        }
         
+        // Quand on est ici, input est forcément valide
+        char letter = input[0];
+
+        _usedLettersList.Add(letter);
+        usedLetters.text = string.Join(", ", _usedLettersList);
+
+        bool letterIsFound = false;
+        for (int i = 0; i < _wordLength; i++)
+        {
+            if (letter == WordToFind[i])
+            {
+                _guessingWord[i] = letter;
+                letterIsFound = true;
+            }
+        }
+
+        hiddenWord.text = new string(_guessingWord);
+
+        if (!letterIsFound)
+        {
+            _attemptsLeftCount--;
+            attemptsLeft.text = attemptsLeftLabel + _attemptsLeftCount.ToString();
+        }
+
+        CheckGameOver();
+    }
+
+    void CheckGameOver()
+    {
+        if (_attemptsLeftCount <= 0) // Le joueur a perdu
+        {
+            // Donne la solution au joueur
+            hiddenWord.text = WordToFind;
+            
+            //Désactive le champ de texte + le bouton
+            letterInput.interactable = false;
+            submitButton.interactable = false;
+            letterInput.DeactivateInputField();
+            
+            // Change le texte tentatives restantes en rouge
+            attemptsLeft.color = Color.red;
+        }
+        else if (System.Array.IndexOf(_guessingWord, HidingCharacter) == -1) // le joueur a gagné
+        {
+            //Désactive le champ de texte + le bouton
+            letterInput.interactable = false;
+            submitButton.interactable = false;
+            letterInput.DeactivateInputField();
+            
+            // Change le texte tentatives restantes en rouge
+            attemptsLeft.color = Color.green;
+        }
+    }
+
+    bool ValidateInput(string inputString)
+    {
+        if (inputString.Length != 1 || string.IsNullOrEmpty(inputString))
+        {
+            Debug.LogWarning("Entrée invalide : veuillez n'entrer qu'une seule lettre");
+            return false;
+        }
+
+        if (!char.IsLetter(inputString[0]))
+        {
+            Debug.LogWarning("Entrée invalide : veuillez entrer une lettre alphabétique");
+            return false;
+        }
+
+        if (_usedLettersList.Contains(inputString[0]))
+        {
+            Debug.LogWarning("La lettre a déjà été utilisée");
+            return false;
+        }
+
+        Debug.Log("La chaîne entrée par l'utilisateur est valide !");
+        return true;
     }
 }
